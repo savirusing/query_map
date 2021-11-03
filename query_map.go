7a95ebf0e4a999b7f94a1568769d6a9b6218265b
Onlfinte_test <savirusingap@hotmail.com>
2021-11-03 10:11:48 +0700

    create query map

---
+++ query_map.go
@@ -0,0 +1,44 @@
+package query_map
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type Database struct {
+	DB *sqlx.DB
+}
+
+func SqlInjection(sql string) error {
+	injection := []string{
+		"insert",
+		"update",
+		"delete",
+		"truncate",
+		"alter",
+		"drop",
+		"exec",
+		"create",
+		"grant",
+		"revolk",
+	}
+	for _, v := range injection {
+		if strings.Contains(sql, v) {
+			response := "error : prohibited command"
+			println(response)
+			return errors.New("error : sql contains prohibit command")
+		}
+	}
+	return nil
+}
+
+func (db Database) SqlQuery(sql string) (string error) {
+	err := SqlInjection(sql)
+	if err != nil {
+		return err
+	}
+	db.DB.Ping()
+	return nil
+}
